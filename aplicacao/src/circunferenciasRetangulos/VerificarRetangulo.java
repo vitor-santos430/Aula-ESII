2307c194b0c01e0d56338497a13d5fd34858221e
vitor-santos430 <vitor.santos430@etec.sp.gov.br>
2021-05-01 03:36:20 -0300

    Criando metodo de calculos do retangulo

---
+++ aplicacao/src/circunferenciasRetangulos/VerificarRetangulo.java
@@ -0,0 +1,31 @@
+package circunferenciasRetangulos;
+
+public class VerificarRetangulo {
+	private static double base;
+	private static double altura;
+	private static double perimetro;
+	private static double area;
+	
+	public VerificarRetangulo(double base, double altura) {
+		this.base = base;
+		this.altura = altura;
+		verificarArea();
+		verificarPerimetro();
+	}
+	
+	private static void verificarArea() {
+		area = base * altura;
+	}
+	
+	private static void verificarPerimetro() {
+		perimetro = 2*(base+altura);
+	}
+	
+	public double getAreaRetangulo() {
+		return area;
+	}
+	
+	public double getPerimetroRetangulo() {
+		return perimetro;
+	}
+}
